3690cfb3d6905b58cdebafa6a6d23979c78fb602
Silverwolf25 <tflatau@emich.edu>
2024-11-18 14:14:58 -0500

    changes

---
+++ Assets/Scripts/Entities/Player.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class Player : Character
+{
+    public HealthBar healthBarPrefab;
+    HealthBar healthBar;
+  
+
+    void Start()
+    {
+        hitPoints.value = startingHitPoints;
+        healthBar = Instantiate(healthBarPrefab);
+        healthBar.character = this;
+    }
+
+    // void OnTriggerEnter2D(Collider2D collision)
+    // {
+    //     if (collision.gameObject.CompareTag("CanBePickedUp"))
+    //     {
+    //         Item hitObject = collision.gameObject.GetComponent<Consumable>().item;
+
+    //         if (hitObject != null)
+    //         {
+    //             bool shouldDisappear = false;
+
+    //             switch (hitObject.itemType)
+    //             {
+    //                 case Item.ItemType.COIN:
+
+    //                 shouldDisappear = inventory.AddItem(hitObject);
+    //                 break;
+    //                 case Item.ItemType.HEALTH:
+
+    //                 shouldDisappear = AdjustHitPoints(hitObject.quantity);
+    //                 break;
+    //                 default:
+    //                 break;
+    //             }
+
+    //             if (shouldDisappear)
+    //             {
+    //                 collision.gameObject.SetActive(false);
+    //             }
+    //         }
+    //     }
+    // }
+
+    public bool AdjustHitPoints(int amount)
+    {
+        if (hitPoints.value < maxHitPoints)
+        {
+            hitPoints.value = hitPoints.value + amount;
+            return true;
+        }
+        return false;
+    }
+}
